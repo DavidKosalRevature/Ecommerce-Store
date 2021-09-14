6763a344c02b544fc0b1c7a1b6c0d4fd6faa7894
LeylaShams <leyla.shams@revature.net>
2021-09-13 22:03:02 -0700

    User table created in db suuccessfully

---
+++ project2-spring/src/main/java/com/revature/Project2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2SpringApplication.class, args);
+	}
+
+}
